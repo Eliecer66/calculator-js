468c039b29d131c71e69cc7121a6f069ad7b8a9e
Jorge Acosta <jorge1071@gmail.com>
2022-03-11 17:24:58 +0100

    get elements by tags

--- operaciones.js
+++ operaciones.js
@@ -1,49 +1,69 @@
-/*
-const number_1 = document.querySelector("#button_1").value;
-const number_2 = document.querySelector("#button_2").value;
-const number_3 = document.querySelector("#button_3").value;
-const number_4 = document.querySelector("#button_4").value;
-const number_5 = document.querySelector("#button_5").value;
-const number_6 = document.querySelector("#button_6").value;
-const number_7 = document.querySelector("#button_7").value;
-const number_8 = document.querySelector("#button_8").value;
-const number_9 = document.querySelector("#button_9").value;
-const number_0 = document.querySelector("#button_0").value;
-const add = document.querySelector("#button_add").value;
-const subtract = document.querySelector("#button_subtract").value;
-
-number_1.addEventListener("click", function(){
-    first_value = number_1;
-})
-number_2.addEventListener("click", function(){
-    first_value = number_2;
-})
-*/
 function set_term() {
-    while (term != '+' || term != '-') {
-
-    }
+  while (term != "+" || term != "-") {}
 }
 
 function calculation() {
-    let firstTerm = document.querySelector("#button_1").value;
-    let secondTerm = document.querySelector("#button_2").value;
-    let operator = document.querySelector("#button_add").value;
-    let one = parseInt(firstTerm, 10);
-    let two = parseInt(secondTerm, 10);
-    let final = 0;
+  let firstTerm = document.querySelector("#button_1").value;
+  let secondTerm = document.querySelector("#button_2").value;
+  let operator = document.querySelector("#button_add").value;
+  let one = parseInt(firstTerm, 10);
+  let two = parseInt(secondTerm, 10);
+  let final = 0;
 
-    if (operator === '+') {
-        final = (one + two);
-        //document.querySelector(".display").textContent = final;
-        let display = document.querySelector(".display");
-        display.innerHTML = `${final}`;
-    } else if (operator === '-') {
-        final = (one - two);
-        document.querySelector(".display").textContent = final;
-    }
+  if (operator === "+") {
+    final = one + two;
+    //document.querySelector(".display").textContent = final;
+    let display = document.querySelector(".display");
+    display.innerHTML = `${final}`;
+  } else if (operator === "-") {
+    final = one - two;
+    document.querySelector(".display").textContent = final;
+  }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('#button_0').onclick = calculation;
+const inputs = document.querySelectorAll("form input");
+
+const handleClick = function (event) {
+  console.log(event.target.value);
+};
+
+inputs.forEach(function (input) {
+  input.addEventListener("click", handleClick);
 });
+
+// function declaration
+function name() {}
+
+// anonymous function
+// function() {}
+
+// function assignment
+var functionAA = function () {};
+
+// inputs.addEventListener("click", function() {
+//     // code
+// });
+
+// if (bloque) {
+//     const myVar = 1;
+// } else {
+//     // code
+// }
+
+// for (let i = 0; i<algo; i++) {
+//     // code
+// }
+
+// function miFunc() {
+//     const myVar = 1;
+//     function innerFunc() {
+//         myVar
+//     }
+
+//     return innerFunc;
+// }
+
+// const myObject = {
+//     name: 'jorge',
+//     myMethod: function() { console.log('buu')}
+// };
