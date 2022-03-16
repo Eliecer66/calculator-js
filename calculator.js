ec9126eebda1b4fc2c74f2389b0838046651178d
Jorge <jorge.eacostadiaz@gmail.com>
2022-03-16 15:04:07 -0300

    Upload changes

--- calculator.js
+++ calculator.js
@@ -1,73 +1,128 @@
 const PLUS = '+'; 
 const SUBTRACT = '-';
-const DELETE = "delete";
+const MULTIPLY = '*';
+const DIVIDE = '/';
+const DELETE = "⌂";
 const EQUAL = '=';
-const DISPLAYzERO = `0`;
-const EMPTYsTRING = "";
-const EMPTYcHARACTER = '';
+const DISPLAY_ZERO = `0`;
 const BASE = 10;
-const OUTCOME = document.querySelectorAll("input");
-/*
- * I don´t know if the behavior of this object is like a struct:
- * Questions: 
- *  It has to be declared first as variable before use it or not?
- *  Also I dont't know if I can set the variables that are on it.
- *  Because of it's working, I'm not sure.
+const CLEAR =  'C'
+const NUMBERS = document.querySelectorAll(".numbers");
+const ACTIONS = document.querySelectorAll(".actions");
+const OPERATORS = document.querySelectorAll(".operators");
+/* How the calculator works.
+ * 1. When a number is joined, it saved into a string called term.
+ * 2. Only we can delete a number if is in the join face. 
+ * 3. The string shows how is going older terms. 
+ * 4. The operators are limited by the current number that the user wants to join 
  */
-let count = {
-  term: EMPTYsTRING,
-  firstTerm: EMPTYsTRING,
-  secondTerm: EMPTYsTRING,
-  operator: EMPTYsTRING,
-  total: EMPTYsTRING,
-  display: document.querySelector(".display")
+let display = document.querySelector(".counts");
+let livesResult = document.querySelector(".lives-result");
+const calculator = {
+  term: "",
+  current: "",
+  operator: "",
+  nextOperator: "",
+  total: 0,
 }; 
 /*
- *PRE: It took globals variables from the general scope. Theses variables has to be charged previously.
- *POST: It calculates the operation between two integers.
- *NOTE: I have to improve this function, It has to be able to calculate more operations. 
+ *PRE-CONDITIONS: It needed two terms.
+ *POST-CONDITIONS:This function return the final result. 
  */
-function calculation() {
-  let one = parseInt(count.firstTerm, BASE);
-  let two = parseInt(count.secondTerm, BASE);
+function calculation(firstTerm, secondTerm) {
+  let one = parseInt(firstTerm, BASE);
+  let two = parseInt(secondTerm, BASE);
   let final = 0;
-  if (count.operator === PLUS) {
-      final = one + two;
-  } else if (count.operator === SUBTRACT) {
+  if (calculator.operator === PLUS) {
+    final = one + two;
+  } 
+  if (calculator.operator === SUBTRACT) {
+    if (one === 0) {
+      final = two;   
+    }else {
       final = one - two;
+    }
+  }
+  if (calculator.operator === MULTIPLY) {
+    if (one === 0) {
+      one = 1;
+    }    
+    final = one * two;
+  }
+  if (calculator.operator === DIVIDE) {
+    if (one === 0) {
+      final = two; 
+    }else {
+      final = one / two;
+    }
   }
   return final;
 }
 /*
- *PRE: This function is activated if the eventListener is true.
- *POST: Store the elements selected in the calculator by the user.
- *NOTE: This function has to be modularized and scalable, so it has to improve.
+ *PRE-CONDITIONS: This function get information from a event listener.
+ *POST-CONDITIONS: It Saves the numbers when are clicked.
  */
-const SAVEcHARACTER = function(event) {  
+const saveNumber = function(event) {  
   let character = event.target.value;
-  if (character != SUBTRACT && character != PLUS && character != EQUAL && character != DELETE) {
-      count.term += event.target.value;
-      count.display.innerHTML = `${count.term}`;
-  } else if (character === SUBTRACT || character === PLUS) {
-      count.operator = character;
-      count.firstTerm =  count.term;
-      count.term = EMPTYcHARACTER;
-      count.display.innerHTML = DISPLAYzERO;
-  } else if (character === EQUAL) {
-      count.secondTerm = count.term;
-      final = calculation();
-      count.display.innerHTML = `${final}`;
-  } else if (character == DELETE) {
-      count.firstTerm = EMPTYsTRING;
-      count.secondTerm = EMPTYsTRING;
-      count.term = EMPTYsTRING;
-      count.display.innerHTML = DISPLAYzERO;
-  }
+  calculator.term += character;
+  calculator.current += character;
+  livesResult.innerHTML = calculator.current;
 }
+NUMBERS.forEach(function(input) {
+  input.addEventListener("click", saveNumber)
+});
 /*
- *PRE: It took a bunch of elements from the DOM in this case inputs, to add a event listener to them. Because it is a calculator. 
- *POST: If de event is true, It executes process to get a solution.   
+ *PRE-CONDITIONS: This function gets information from global variables filled previously.
+ *POST-CONDITIONS: It Calculates a number.
  */
-OUTCOME.forEach(function(input){
-  input.addEventListener("click", SAVEcHARACTER)
+const operate = function (event) {
+  if (calculator.operator === "") {
+    calculator.operator = event.target.value;
+    calculator.nextOperator = calculator.operator;
+    calculator.term += calculator.operator;
+  }else {
+    calculator.nextOperator = event.target.value;
+    calculator.term += calculator.nextOperator;
+  }
+  calculator.total = calculation(calculator.total, calculator.current);
+  calculator.current = "";
+  calculator.operator = calculator.nextOperator;
+  display.innerHTML = calculator.term;
+  livesResult.innerHTML = calculator.total;
+};
+/*
+ *PRE-CONDITIONS: This function gets information from global variables filled previously.
+ *POST-CONDITIONS: It does three actions when it activates by event listener. 
+ */
+const doActions = function (event) {
+  let character = event.target.value; 
+  if(character === EQUAL) {
+    calculator.total = calculation(calculator.total, calculator.current);
+    livesResult.innerHTML = calculator.total;
+    display.innerHTML = "";   
+  }
+  if (character === CLEAR) {
+    calculator.current = "";
+    calculator.operator = "";
+    calculator.nextOperator = "";
+    calculator.term = "";
+    calculator.total = 0;
+    display.innerHTML = 0;
+    livesResult.innerHTML = 0;
+  }
+  if (character === DELETE) {
+    let firstTerm = calculator.term; 
+    let number = calculator.current;
+    let box = number.slice(0,-1); 
+    let boxOne = firstTerm.slice(0,-1);
+    calculator.term = boxOne; 
+    calculator.current = box;
+    livesResult.innerHTML = calculator.current;
+  }
+};
+OPERATORS.forEach(function(operators) {
+  operators.addEventListener("click", operate)
+});
+ACTIONS.forEach(function(actions) {
+  actions.addEventListener("click", doActions)
 });
