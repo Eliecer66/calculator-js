af05fe87a5c494abbce193fbed605d0bf0db9cf1
Jorge <jorge.eacostadiaz@gmail.com>
2022-07-15 15:29:45 -0300

    Complete calculator

--- calculator.js
+++ calculator.js
@@ -1,146 +1,192 @@
-const PLUS = '+'; 
-const SUBTRACT = '-';
-const MULTIPLY = '*';
-const DIVIDE = '/';
-const DELETE = 'x';
-const EQUAL = '=';
-const DISPLAY_ZERO = '0';
-const BASE = 10;
-const CLEAR =  'C'
-const numbers = document.querySelectorAll(".numbers");
-const actions = document.querySelectorAll(".actions");
+const PLUS      = '+'; 
+const SUBTRACT  = '-';
+const MULTIPLY  = '*';
+const DIVIDE    = '/';
+const DELETE    = 'e';
+const EQUAL     = '=';
+const EMPTY     = '0';
+const BASE      = 10;
+const CLEAR     =  'c'
+const numbers   = document.querySelectorAll(".numbers");
+const actions   = document.querySelectorAll(".actions");
+const equal     = document.querySelector("#equal");
 const operators = document.querySelectorAll(".operators");
-/* How the calculator works.
- * 1. When a number is joined, it saved into a string called term.
- * 2. Only we can delete a number if is in the join face. 
- * 3. The string shows how is going older terms. 
- * 4. The operators are limited by the current number that the user wants to join 
- */
-let display = document.querySelector(".counts");
+let display     = document.querySelector(".counts");
 let livesResult = document.querySelector(".lives-result");
+const sign      = [PLUS, SUBTRACT, MULTIPLY, DIVIDE];
+
+//Global variables
 const calculator = {
-  term: "",
-  current: "",
-  operator: "",
-  nextOperator: "",
-  total: 0,
-}; 
+  container:[],
+  term: '',
+  current: ''
+};
+
 /*
- *PRE-CONDITIONS: It needed two terms.
- *POST-CONDITIONS:This function return the final result.
- *NOTE: In the subtract's case is necessary the "if" because otherwise, the subtract can't be possible.  
+ *PRE-CONDITIONS: This function gets information from a event listener.
+ *POST-CONDITIONS: It saves the numbers when are clicked.
  */
-function calculation(total, current) {
-  const one = parseInt(total, BASE);
-  const two = parseInt(current, BASE);
-  let final = 0;
-  if (!total) {
-    return current;
+const saveNumber = function(event, element) {
+  if (calculator.term.length < 17) {
+    let character       =  element || event.target.value;
+    calculator.term    += character;
+    calculator.current += character;
+    display.innerHTML   = calculator.term;
+
   }
-  if (calculator.operator === PLUS) {
-    final = one + two;
-  } 
-  if (calculator.operator === SUBTRACT) {
-    final = one - two;
-  }
-  if (calculator.operator === MULTIPLY) {
-    final = one * two;
-  }
-  if (calculator.operator === DIVIDE) {
-    if (two === 0) {
-      final = "FATAL ERROR";
-    } else {
-      final = one / two;
-    }
-  }
-  return final;
 }
-/*
- *PRE-CONDITIONS: This function get information from a event listener.
- *POST-CONDITIONS: It Saves the numbers when are clicked.
- */
-const saveNumber = function(event) {  
-  const character = event.target.value;
-  calculator.term += character;
-  calculator.current += character;
-  livesResult.innerHTML = calculator.current;
-}
-/*
- *PRE-CONDITIONS: This function receive two variables that are going to be used to change two global variables.
- *POST-CONDITIONS: Change the state of the display.
- */
-function showDisplay(stringOperations, total) {
-  display.innerHTML = stringOperations;
-  livesResult.innerHTML = total;
-}
-/*
- *PRE-CONDITIONS: Receive one variable.
- *POST-CONDITIONS: Change the state of two variables.
- */
-function reassignedVariables(operator) {
-  calculator.current = "";
-  calculator.operator = operator;
-}
+
 /*
  *PRE-CONDITIONS: This function gets information from global variables filled previously.
  *POST-CONDITIONS: It Calculates a number.
  */
-const operate = function (event) {
-  const value = event.target.value;
-  if (!calculator.operator) {
-    calculator.operator = value;
-    calculator.nextOperator = calculator.operator;
-    calculator.term += calculator.operator;
-    calculator.total = calculator.current;
-    reassignedVariables(calculator.nextOperator);
-    showDisplay(calculator.term, calculator.total);
-  } else {
-    calculator.nextOperator = value;
-    calculator.term += calculator.nextOperator;
-    calculator.total = calculation(calculator.total, calculator.current);
-    reassignedVariables(calculator.nextOperator);
-    showDisplay(calculator.term, calculator.total);
-  }
+const operate = function(event, element) {
+
+  let top     = calculator.container.length;
+  const value = element || event.target.value;
+  
+  calculator.container[top]     = calculator.current;
+  calculator.container[top + 1] = value;
+  calculator.term              += value;
+  calculator.current            = '';
+  calculator.operator           = '';
+  display.innerHTML             = calculator.term;
 };
+
 /*
  *PRE-CONDITIONS: This function gets information from global variables filled previously.
  *POST-CONDITIONS: It does three actions when it activates by event listener. 
  */
-const doActions = function (event) {
-  const character = event.target.value; 
-  if(character === EQUAL) {
-    calculator.total = calculation(calculator.total, calculator.current);
-    livesResult.innerHTML = calculator.total;
-    display.innerHTML = "0";   
-    calculator.current = 0;
-  }
-  if (character === CLEAR) {
-    calculator.current = "";
-    calculator.operator = "";
-    calculator.nextOperator = "";
-    calculator.term = "";
-    calculator.total = 0;
-    display.innerHTML = 0;
-    livesResult.innerHTML = 0;
-  }
-  if (character === DELETE) {
-    const firstTerm = calculator.term;
-    const number = calculator.current;
-    calculator.term = firstTerm.slice(0,-1);
-    calculator.current = number.slice(0,-1);
-    livesResult.innerHTML = calculator.current;
+const wipeAction = function (event,element) {
 
-    if(calculator.current.length === 0) {
+  const character = element || event.target.value;
+
+  if (character === CLEAR) {
+    calculator.container.length = 0;
+    calculator.current          = '';
+    calculator.term             = '';
+    display.innerHTML           = 0;
+    livesResult.innerHTML       = 0;
+  }
+  
+  if (character === DELETE && calculator.term.length > 0) {
+    const firstTerm       = calculator.term;
+    const number          = calculator.current;
+    calculator.term       = firstTerm.slice(0,-1);
+    calculator.current    = number.slice(0,-1);
+    display.innerHTML     = calculator.term;
+    
+    if (calculator.current.length === 0) {
+      display.innerHTML     = 0;
       livesResult.innerHTML = 0;
     }
   }
 };
-numbers.forEach(function(input) {
+
+/*
+ *PRE-CONDITIONS: It needed two terms.
+ *POST-CONDITIONS:This function return the final result.
+ */
+function solution(first, second, operator) {
+
+  const a = parseInt(first, BASE);
+  const b = parseInt(second, BASE);
+  var total;
+
+  switch (operator) {
+    case PLUS:
+      total = a + b;
+      break;
+    case SUBTRACT:
+      total = a - b;
+      break;
+    case MULTIPLY:
+      total = a * b;
+      break;
+    case DIVIDE:
+      if (b === 0) {
+        total = "FATAL ERROR";
+      } else {
+        total = a / b;
+      }
+      break;
+  }
+  return total || first;
+}
+
+/*PRE-CONDITIONS:
+ *POST-CONDITIONS: It Takes the elements from the global varables and solves the operations
+ in order.
+ */
+const resolution = function() {
+  var first, second, operator, total;
+  var i = 0;
+  calculator.container.push(calculator.current);
+  console.log(calculator.container);
+
+  for (i ; i < calculator.container.length; i++) {
+    if (i === 0) {
+      first    = calculator.container[i];
+      i++;
+      operator = calculator.container[i];
+      i++;
+      second   = calculator.container[i];
+      total    = solution(first,second, operator);
+    } else {
+      first    = total;
+      operator = calculator.container[i];
+      i++;
+      second   = calculator.container[i];
+      total    = solution(first,second, operator);
+    }
+    console.log(first);
+    console.log(operator);
+    console.log(second);
+  }
+  console.log(total);
+  console.log("------------");
+  calculator.term            += calculator.current;
+  livesResult.innerHTML       = total;
+  display.innerHTML = ' ';
+  calculator.term             = total;
+  calculator.container.length = 0;
+  calculator.current          = total;
+}
+
+numbers.forEach(function (input) {
   input.addEventListener("click", saveNumber)
 });
-operators.forEach(function(operators) {
+
+operators.forEach(function (operators) {
   operators.addEventListener("click", operate)
 });
-actions.forEach(function(actions) {
-  actions.addEventListener("click", doActions)
+
+actions.forEach(function (actions) {
+  actions.addEventListener("click", wipeAction)
+});
+
+equal.addEventListener('click', resolution);
+
+document.addEventListener('keydown', function (event) {
+  let keyName = event.key;
+
+  if (!isNaN(keyName)) {
+    saveNumber(EMPTY, keyName);
+  }
+  if (keyName === 'Backspace') {
+    event.preventDefault();
+    keyName = DELETE;
+    wipeAction(EMPTY,keyName);
+  }
+  if (keyName === CLEAR) {
+    wipeAction(EMPTY,keyName);
+  }
+  if (keyName === 'Enter' || keyName === EQUAL) {
+    resolution();
+  }
+  sign.forEach(element => {
+    if (keyName === element) {
+      operate(EMPTY, keyName);
+   }
+ });
 });
